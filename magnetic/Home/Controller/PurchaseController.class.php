f0da8becb4efc69ba211d44f51f0f49431ced9b5
sdhylj <sdhylj@163.com>
2016-10-14 16:09:12 +0800

    我的分页

--- magnetic/Home/Controller/PurchaseController.class.php
+++ magnetic/Home/Controller/PurchaseController.class.php
@@ -188,10 +188,41 @@ class PurchaseController extends BaseController {
 	//我的采购
 	public function mypurchase(){
 		$userid = I('session.userid',0);
-		$tb = new OrderPurchaseModel();
-		$ret = $tb->getMyPurchase($userid);
+		//$tb = new OrderPurchaseModel();
+		// $ret = $tb->getMyPurchase($userid);
 		// dump($ret);
-		$this->assign('purchaselist',$ret);
+		
+		/*分页控制*/
+		$page_num = I('get.page_num',0);  //当前页(一直是0)
+        $pagesize = C('PAGESIZE');  //配置文件读取每页行数
+		$tb = M();
+		$ret = $tb->procedure('call p_mypurchase('.$userid.',0,'.$pagesize.',"'.$query.'")');  //查询记录总数,页数
+        $rowcount = $ret[0][0]['rowcount'];//记录总数
+		$end_page = $ret[0][0]['pagecount'];//last page
+        //查询记录如果小于页面显示数量，则标记页码div 不显示。反之展示。
+        $show_flag = $rowcount<$pagesize?0:1;
+		$Page = new \Think\cPage($rowcount,$pagesize);// 实例化分页类 传入总记录数和每页显示的记录数
+		$show = $Page->show();// 分页显示输出
+		$show = str_replace("<div>",'',$show);
+        $show = str_replace("</div>",'',$show);
+        $first = floor($Page->firstRow);
+		$ret = $tb->procedure('call p_mypurchase('.$userid.','.$first.','.$pagesize.',"'.$query.'")');
+		//$end_page=ceil($rowcount/$pagesize);
+		// dump($ret);
+        if(count($ret)==1){
+            $this->assign('str_null',"暂无数据");
+            $this->assign('data_count',"0");
+        }else{
+            $this->assign('data_count',$rowcount);
+        }
+		//模板传值
+        $this->assign('page',$show);  //页码
+        $this->assign('end_page',$end_page);  //末页
+        $this->assign('show_flag',$show_flag);  //?标记
+        $this->assign('json_arr',$ret[1]);  //数据集
+		/*分页控制*/
+		
+		// $this->assign('purchaselist',$ret);
 		$this->display();
 	}
 }
