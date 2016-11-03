f10a18726825435eab6234622d44646df5a6c026
sdhylj <sdhylj@163.com>
2016-11-03 22:01:34 +0800

    首页填充ss

--- magnetic/Home/Controller/IndexController.class.php
+++ magnetic/Home/Controller/IndexController.class.php
@@ -30,12 +30,20 @@ class IndexController extends BaseController {
 		$this->assign('siteinfo',$siteinfo);  //网站信息数组，配置文件中读取。
 		
 		$order = new OrderSupplyModel();
+		$rtn = $order->getSupplyList(1);  //供应/现货信息
+		$this->assign('supplyF1',$rtn);
+		$rtn = $order->getSupplyList(2);  //供应/现货信息
+		$this->assign('supplyF2',$rtn);
 		$rtn = $order->getSupplyList(3);  //供应/现货信息
-		$this->assign('supply',$rtn);
+		$this->assign('supplyF3',$rtn);
 		//dump($rtn);
 		$order = new OrderPurchaseModel();
-		$rtn = $order->getPurchaseList(5);  //采购信息
-		$this->assign('purchase',$rtn);
+		$rtn = $order->getPurchaseList(1);  //采购信息
+		$this->assign('purchaseF1',$rtn);
+		$rtn = $order->getPurchaseList(2);  //采购信息
+		$this->assign('purchaseF2',$rtn);
+		$rtn = $order->getPurchaseList(3);  //采购信息
+		$this->assign('purchaseF3',$rtn);
 		//dump($rtn);
 		$this->display();
     }
